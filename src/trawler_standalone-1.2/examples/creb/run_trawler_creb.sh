3df2631edbc9869802d5a928306a05033b78eaa8
Simon van Heeringen <simon.vanheeringen@gmail.com>
2010-07-29 16:36:42 +0200

    Removed trawler examples and MoAn backgrounds

--- src/trawler_standalone-1.2/examples/creb/run_trawler_creb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# cd trawler/
-perl bin/trawler.pl -dir_id expl2 -sample examples/creb/creb.fa -background examples/creb/creb_background.fa
-
-exit 0
--- src/trawler_standalone-1.2/examples/e2f/run_trawler_e2f.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# cd trawler/
-perl bin/trawler.pl -dir_id expl2 -sample examples/e2f/e2f.fa -background examples/e2f/e2f_background.fa
-
-exit 0
