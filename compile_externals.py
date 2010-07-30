ebe824418d4a1c712b6c192f8857f1b88858e9c5
Simon <simon.vanheeringen@gmail.com>
2010-07-30 14:12:01 +0200

    Script to compile included motif tools.

---
+++ compile_externals.py
@@ -0,0 +1,73 @@
+import sys
+import os
+from subprocess import *
+from platform import platform
+
+def compile_simple(name):
+	path = "src/%s" % name
+	
+	if not os.path.exists(path):
+		return
+	
+	try:
+		Popen("gcc", stdout=PIPE, stderr=PIPE).communicate()
+	except:
+		return
+	
+	Popen(["gcc","-lm","-o%s" % name, "%s.c" % name], cwd=path, stdout=PIPE).communicate()
+	if os.path.exists(os.path.join(path, name)):
+		return True
+
+def compile_configmake(name, binary):
+	path = "src/%s" % name
+
+	if not os.path.exists(path):
+		return
+
+	Popen(["./configure"], cwd=path, stdout=PIPE, stderr=PIPE).communicate()
+	Popen(["make"], cwd=path, stdout=PIPE, stderr=PIPE).communicate()
+
+	if os.path.exists(os.path.join(path, binary)):
+		return True
+
+def compile_moan():
+	# Fix the Makefile 
+	from re import compile
+	p = compile(r'^CC=gcc.*$')
+	makefile = "src/MoAn/Makefile"	
+	if not os.path.exists(makefile):
+		return 
+
+	lines = open(makefile).readlines()
+	
+	f = open(makefile, "w")	
+	for line in lines:
+		if p.match(line):
+			if platform().find("fedora") != -1:
+				f.write("CC=gcc34\n")
+			elif platform().find("gentoo") != -1:
+				f.write("CC=gcc-4.1\n")
+			else:
+				f.write(line)
+		else:
+			f.write(line)
+	f.close()
+	
+	Popen(["make"], cwd="src/MoAn/", stdout=PIPE, stderr=PIPE).communicate()
+	if os.path.exists("src/MoAn/moan"):
+		return True
+
+def compile_perl(path):
+	if not os.path.exists(path):
+		return
+	Popen(["perl", "Makefile.PL"], cwd=path, stdout=PIPE).communicate()
+	Popen(["make"], cwd=path, stdout=PIPE).communicate()
+	return True
+	
+
+#print compile_simple("BioProspector")
+#print compile_simple("MDmodule")
+#print compile_configmake("meme_4.4.0", "src/meme.bin")
+#print compile_configmake("GADEM_v1.3", "src/gadem")
+#print compile_moan()
+print compile_perl("Algorithm-Cluster-1.49")
