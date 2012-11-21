0029ede193366dda44a858ce98c6902ebdc7aaad
Simon van Heeringen <simon.vanheeringen@gmail.com>
2012-11-21 16:32:47 +0100

    Parallel version of pwmscan.py

--- scripts/pwmscan.py
+++ scripts/pwmscan.py
@@ -1,74 +1,78 @@
 #!/usr/bin/python
-# Copyright (c) 2009-2010 Simon van Heeringen <s.vanheeringen@ncmls.ru.nl>
+# Copyright (c) 2009-2012 Simon van Heeringen <s.vanheeringen@ncmls.ru.nl>
 #
 # This module is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License, see the file COPYING included with this 
 # distribution.
 
 import sys
-from gimmemotifs.fasta import Fasta
-from gimmemotifs.c_metrics import pwmscan
-from gimmemotifs.utils import *
-from gimmemotifs.motif import *
-from optparse import OptionParser,TitledHelpFormatter
-from math import log
 import re
+from optparse import OptionParser,TitledHelpFormatter
+import pp
 
-VERSION = "1.03"
+from gimmemotifs.motif import *
 
-nreport = 1 
+VERSION = "1.1"
+NREPORT = 1 
+MAX_CPUS = 16
 
 usage = "usage: %prog -i <FILE> [optional arguments]"
 parser = OptionParser(version=VERSION, usage=usage, formatter=TitledHelpFormatter(max_help_position=40, short_first=1))
 parser.add_option("-i", "--input", dest="inputfile", help="FASTA-formatted inputfile", metavar="FILE")
-parser.add_option("-p", "--pwm", dest="pwmfile", help="specify your own PWM file instead of MDmodule file", metavar="FILE")
-parser.add_option("-n", "--nreport", dest="nreport", help="report the N best matches", metavar="N")
+parser.add_option("-p", "--pwm", dest="pwmfile", help="PWM file with motifs", metavar="FILE")
+parser.add_option("-n", "--nreport", dest="nreport", help="report the N best matches", metavar="N", default=NREPORT, type=int)
 parser.add_option("-r", "--norc", dest="scan_rc", help="don't scan reverse complement (- strand)", default=True, action="store_false")
-parser.add_option("-c", "--cutoff", dest="cutoff", help="motif score cutoff (fraction of maxscore, default 0.9)", metavar="", default=0.9)
+parser.add_option("-c", "--cutoff", dest="cutoff", help="motif score cutoff (fraction of maxscore, default 0.9)", metavar="", default=0.9, type=float)
 parser.add_option("-b", "--bed", action="store_true", dest="bed", default=False, help="output bed format")
 
 (options, args) = parser.parse_args()
 
-if not (options.inputfile and (options.pwmfile or options.mdmodulefile)):
+if not (options.inputfile and options.pwmfile):
 	parser.print_help()
 	sys.exit(0)
 
 inputfile = options.inputfile
-
-if options.nreport:
-	nreport = int(options.nreport)
-
-cutoff = float(options.cutoff)
-
-motifs = pwmfile_to_motifs(options.pwmfile)
-
+nreport = options.nreport
+cutoff = options.cutoff
 bed = options.bed
 
-f = Fasta(inputfile)
-strandmap = {-1:"-",1:"+"}
-for (id,seq) in f.items():
-	for motif in motifs:
-		pwm = motif.pwm
-		c =  motif.pwm_min_score() + (motif.pwm_max_score() - motif.pwm_min_score()) * cutoff 
-		result = pwmscan(seq.upper(), pwm, c, nreport, options.scan_rc)
-		for (score, pos, strand) in result:
+def scan_fa_with_motif(fname, motif, cutoff, nreport, bed):
+	from gimmemotifs.fasta import Fasta
+	f = Fasta(fname)
+	strandmap = {-1:"-",1:"+"}
+	
+	result = motif.pwm_scan_all(f, cutoff, nreport)
+	for seq_id, matches in result.items():
+		for (pos, score, strand) in matches:
 			if bed:
-				first = id.split(" ")[0]	
-				(chr,loc) = first.split(":")
+				first = seq_id.split(" ")[0]	
+				(chrom,loc) = first.split(":")
 				if loc:
 					(start, end) = map(int, loc.split("-"))
-					print "%s\t%s\t%s\t%s" % (chr, start + pos, start + pos + len(pwm) , score)
+					print "%s\t%s\t%s\t%s" % (chrom, start + pos, start + pos + len(motif) , score)
 				else:
-					print "%s\t%s\t%s\t%s" % (id, pos, pos +  len(pwm), score)
+					print "%s\t%s\t%s\t%s" % (seq_id, pos, pos +  len(motif), score)
 			else:
 				print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tmotif_name \"%s\" ; motif_instance \"%s\"" % (
-				id, 
-				"pwmscan", 
-				"misc_feature", 
-				pos, pos + len(pwm) , 
-				score, 
-				strandmap[strand], 
-				".", 
-				motif.id, 
-				seq[pos: pos + len(pwm)]
-			)
+					seq_id, 
+					"pwmscan", 
+					"misc_feature", 
+					pos, pos + len(motif) , 
+					score, 
+					strandmap[strand], 
+					".", 
+					motif.id, 
+					f[seq_id][pos: pos + len(motif)]
+				)
+
+job_server = pp.Server(secret="beetrootsoup")
+if job_server.get_ncpus() > MAX_CPUS:
+	job_server.set_ncpus(MAX_CPUS)
+
+jobs = []
+motifs = pwmfile_to_motifs(options.pwmfile)
+for motif in motifs:
+	jobs.append(job_server.submit(scan_fa_with_motif, (inputfile, motif, cutoff, nreport, bed), (),()))
+
+for job in jobs:
+	job()
