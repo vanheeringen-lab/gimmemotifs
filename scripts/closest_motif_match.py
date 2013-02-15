bf0113f41b1e84856209dd673beba0d69eccc36b
Simon <simon.vanheeringen@gmail.com>
2013-02-15 15:23:25 +0100

    Added p-value to closest motif match

--- scripts/closest_motif_match.py
+++ scripts/closest_motif_match.py
@@ -19,13 +19,15 @@ if not (options.pwmfile and options.dbpwmfile):
 	parser.print_help()
 	sys.exit()
 
-sample = pwmfile_to_motifs(options.pwmfile)
-db = pwmfile_to_motifs(options.dbpwmfile)
+sample = dict([(m.id, m) for m in pwmfile_to_motifs(options.pwmfile)])
+db = dict([(m.id, m) for m in pwmfile_to_motifs(options.dbpwmfile)])
 
 mc = MotifComparer()
 
-result = mc.get_closest_match(sample, db, "partial", "wic", "mean")
+print "Motif\tMatch\tScore\tP-value"
+result = mc.get_closest_match(sample.values(), db.values(), "partial", "wic", "mean")
 for motif, match in result.items():
-	print "%s\t%s\t%0.2f" % (motif, match[0], match[1][0])
+	pval, pos, orient = mc.compare_motifs(sample[motif], db[match[0]], "partial", "wic", "mean", pval=True)
+	print "%s\t%s\t%0.2f\t%0.3e" % (motif, match[0], match[1][0], pval)
 
 #print "%s\t%s\t%0.2e" % (motif.id, result[0].id, 1 - result[1][0])
