52164524b6332b3e0ee732f488d827ee1f4bfe49
Simon van Heeringen <simon.vanheeringen@gmail.com>
2013-03-18 13:36:50 +0100

    Plot for matched, aligned motifs

--- gimmemotifs/plot.py
+++ gimmemotifs/plot.py
@@ -3,8 +3,17 @@
 # This module is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License, see the file COPYING included with this 
 # distribution.
-
 """ Various plotting functions """
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from tempfile import NamedTemporaryFile
+
+def axes_off(ax):
+    """Get rid of all axis ticks, lines, etc.
+    """
+    ax.set_frame_on(False)
+    ax.axes.get_yaxis().set_visible(False)
+    ax.axes.get_xaxis().set_visible(False)
 
 def roc_plot(outfile, x, y):
 	import matplotlib.pyplot as plt
@@ -49,4 +58,45 @@ def plot_histogram(values, outfile, xrange=None, breaks=10, title=None, xlabel=N
 		outfile += ".svg"
 	plt.savefig(outfile, format="svg")
 
+def match_plot(plotdata, outfile):
+    """Plot list of motifs with database match and p-value
+    "param plotdata: list of (motif, dbmotif, pval)
+    """
+    fig_h = 2 
+    fig_w = 7
+
+    nrows = len(plotdata)
+    ncols = 2
+    fig = plt.figure(figsize=(fig_w, nrows * fig_h))
+    
+    for i, (motif, dbmotif, pval) in enumerate(plotdata):
+        text = "Motif: %s\nBest match: %s\np-value: %0.2e" % (motif.id, dbmotif.id, pval)
+        
+
+        grid = ImageGrid(fig, (nrows, ncols, i * 2 + 1), 
+                         nrows_ncols = (2,1),
+                         axes_pad=0, 
+                         )
+
+        for j in range(2):  
+            axes_off(grid[j])
+
+        tmp = NamedTemporaryFile(suffix=".png")
+        motif.to_img(tmp.name, format="PNG")
+        grid[0].imshow(plt.imread(tmp.name), interpolation="none")
+        tmp = NamedTemporaryFile(suffix=".png")
+        dbmotif.to_img(tmp.name, format="PNG")
+        grid[1].imshow(plt.imread(tmp.name), interpolation="none")
+
+        ax = plt.subplot(nrows, ncols, i * 2 + 2)
+        axes_off(ax)
+
+        ax.text(0, 0.5, text,
+        horizontalalignment='left',
+        verticalalignment='center') 
+    
+    plt.savefig(outfile, dpi=300, bbox_inches='tight')
+
+
+        
 
