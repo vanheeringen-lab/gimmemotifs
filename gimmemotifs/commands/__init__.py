6834dcb9155e7116da14cc8e5e8c4dfc7db9227a
Simon <simon.vanheeringen@gmail.com>
2013-03-22 12:28:02 +0100

    Dynamic loading of subcommands

--- gimmemotifs/commands/__init__.py
+++ gimmemotifs/commands/__init__.py
@@ -1,6 +1,9 @@
-from match import match
-from motifs import motifs
-from roc import roc
-from maxenr import maxenr
-from cluster import cluster
-from index import index
+import pkgutil
+import os
+
+dirname = os.path.split(__file__)[0]
+
+# Dynamically load all commands
+for importer, cmdname, _ in pkgutil.iter_modules([dirname]):
+    m =  __import__("{0}.{1}".format(__name__, cmdname), globals(), locals(), [cmdname], -1)
+    globals()[cmdname] = getattr(m, cmdname)
