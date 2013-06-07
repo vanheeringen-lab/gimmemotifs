bcdf8f198c36039625305fb5e05ba229c4b44b5a
Simon van Heeringen <simon.vanheeringen@gmail.com>
2013-06-07 09:49:56 +0200

    tabs to spaces

--- gimmemotifs/prediction.py
+++ gimmemotifs/prediction.py
@@ -26,140 +26,140 @@ from gimmemotifs.config import *
 from gimmemotifs.fasta import *
 
 class PredictionResult:
-	def __init__(self, outfile, logger=None, fg_file=None, bg_file=None, job_server=None):
-		self.lock = thread.allocate_lock()
-		self.motifs = []
-		self.finished = []
-		self.logger = logger
-		self.stats = {}
-		self.outfile = outfile
+    def __init__(self, outfile, logger=None, fg_file=None, bg_file=None, job_server=None):
+        self.lock = thread.allocate_lock()
+        self.motifs = []
+        self.finished = []
+        self.logger = logger
+        self.stats = {}
+        self.outfile = outfile
 
-		if fg_file and bg_file:
-			self.fg_fa = Fasta(fg_file)
-			self.bg_fa = Fasta(bg_file)
-			self.do_stats = True
-		else:
-			self.do_stats = False
+        if fg_file and bg_file:
+            self.fg_fa = Fasta(fg_file)
+            self.bg_fa = Fasta(bg_file)
+            self.do_stats = True
+        else:
+            self.do_stats = False
 
-	def add_motifs(self, job, args):
-		# Callback function for motif programs
-		motifs, stdout, stderr = args
-		
-		if self.logger:
-			self.logger.info("%s finished, found %s motifs" % (job, len(motifs))) 
-		
-		for motif in motifs:
-			self.lock.acquire()
-			f = open(self.outfile, "a")
-			f.write("%s\n" % motif.to_pfm())
-			f.close()
-			self.motifs.append(motif)
-			if self.do_stats:
-				self.logger.debug("Starting stats job of motif %s" % motif.id)
-				job_server.submit(motif.stats, (self.fg_fa, self.bg_fa), (), (), self.add_stats, ("%s_%s" % (motif.id, motif.to_consensus()),), group="stats")
-			self.lock.release()
-		
-		self.logger.debug("stdout %s: %s" % (job, stdout))
-		self.logger.debug("stdout %s: %s" % (job, stderr))
-		self.finished.append(job)
+    def add_motifs(self, job, args):
+        # Callback function for motif programs
+        motifs, stdout, stderr = args
+        
+        if self.logger:
+            self.logger.info("%s finished, found %s motifs" % (job, len(motifs))) 
+        
+        for motif in motifs:
+            self.lock.acquire()
+            f = open(self.outfile, "a")
+            f.write("%s\n" % motif.to_pfm())
+            f.close()
+            self.motifs.append(motif)
+            if self.do_stats:
+                self.logger.debug("Starting stats job of motif %s" % motif.id)
+                job_server.submit(motif.stats, (self.fg_fa, self.bg_fa), (), (), self.add_stats, ("%s_%s" % (motif.id, motif.to_consensus()),), group="stats")
+            self.lock.release()
+        
+        self.logger.debug("stdout %s: %s" % (job, stdout))
+        self.logger.debug("stdout %s: %s" % (job, stderr))
+        self.finished.append(job)
 
-	def add_stats(self, motif, stats):
-		self.logger.debug("Stats: %s %s" % (motif, stats))
-		self.stats[motif] = stats
+    def add_stats(self, motif, stats):
+        self.logger.debug("Stats: %s %s" % (motif, stats))
+        self.stats[motif] = stats
 
-	def get_remaining_stats(self):
-		for motif in self.motifs:
-			n = "%s_%s" % (motif.id, motif.to_consensus())
-			if not self.stats.has_key(n):
-				self.logger.info("Adding %s again!" % n)
-				job_server.submit(motif.stats, (self.fg_fa, self.bg_fa), (), (), self.add_stats, ("%s_%s" % (motif.id, motif.to_consensus()),), group="stats")
+    def get_remaining_stats(self):
+        for motif in self.motifs:
+            n = "%s_%s" % (motif.id, motif.to_consensus())
+            if not self.stats.has_key(n):
+                self.logger.info("Adding %s again!" % n)
+                job_server.submit(motif.stats, (self.fg_fa, self.bg_fa), (), (), self.add_stats, ("%s_%s" % (motif.id, motif.to_consensus()),), group="stats")
 
 def pp_predict_motifs(fastafile, outfile, analysis="small", organism="hg18", single=False, background="", tools={}, job_server="", ncpus=8, logger=None, max_time=None, fg_file=None, bg_file=None):
-	
-	config = MotifConfig()
+    
+    config = MotifConfig()
 
-	if not tools:
-		tools = dict([(x,1) for x in config.get_default_params["tools"].split(",")])
-	
-	#logger = logging.getLogger('prediction.pp_predict_motifs')
+    if not tools:
+        tools = dict([(x,1) for x in config.get_default_params["tools"].split(",")])
+    
+    #logger = logging.getLogger('prediction.pp_predict_motifs')
 
-	wmin = 5 
-	step = 1
-	if analysis in ["large","xl"]:
-		step = 2
-		wmin = 6
-	
-	analysis_max = {"xs":5,"small":8, "medium":10,"large":14, "xl":20}
-	wmax = analysis_max[analysis]
+    wmin = 5 
+    step = 1
+    if analysis in ["large","xl"]:
+        step = 2
+        wmin = 6
+    
+    analysis_max = {"xs":5,"small":8, "medium":10,"large":14, "xl":20}
+    wmax = analysis_max[analysis]
 
-	if analysis == "xs":
-		sys.stderr.write("Setting analysis xs to small")
-		analysis = "small"
+    if analysis == "xs":
+        sys.stderr.write("Setting analysis xs to small")
+        analysis = "small"
 
-	if not job_server:
-		job_server = pp.Server(ncpus, secret='pumpkinrisotto')
-	
-	jobs = {}
-	
-	result = PredictionResult(outfile, logger=logger, fg_file=fg_file, bg_file=bg_file, job_server=job_server)
-	
-	# Dynamically load all tools
-	toolio = [x[1]() for x in inspect.getmembers(tool_classes, lambda x: inspect.isclass(x) and issubclass(x, tool_classes.MotifProgram)) if x[0] != 'MotifProgram']
-	
-	# TODO:
-	# Add warnings for running time: Weeder, MoAn, GADEM
-		
-	### Add all jobs to the job_server ###
-	params = {'analysis': analysis, 'background':background, "single":single, "organism":organism}
-	for t in toolio:
-		if tools.has_key(t.name) and tools[t.name]:
-			if t.use_width:
-				for i in range(wmin, wmax + 1, step):
-	 				logger.debug("Starting %s job, width %s" % (t.name, i))
-					job_name = "%s_width_%s" % (t.name, i)
-					params['width'] = i
-					jobs[job_name] = job_server.submit(
-						t.run, 
-						(fastafile, ".", params,), 
-						(tool_classes.MotifProgram,),
-						("gimmemotifs.config",),  
-						result.add_motifs, 
-						(job_name,))
-			else:
-				logger.debug("Starting %s job" % t.name)
-				job_name = t.name
-				jobs[job_name] = job_server.submit(
-					t.run, 
-					(fastafile, ".", params), 
-					(tool_classes.MotifProgram,),
-					("gimmemotifs.config",), 
-					result.add_motifs, 
-					(job_name,))
-		else:
-			logger.debug("Skipping %s" % t.name)
-	
-	### Wait until all jobs are finished or the time runs out ###
-	start_time = time()	
-	try:
-		# Run until all jobs are finished
-		while len(result.finished) < len(jobs.keys()) and (not(max_time) or time() - start_time < max_time):
-			pass
-		if len(result.finished) < len(jobs.keys()):
-			logger.info("Maximum allowed running time reached, destroying remaining jobs")
-			job_server.destroy()
-			result.get_remaining_stats()
-	### Or the user gets impatient... ###
-	except KeyboardInterrupt, e:
-		# Destroy all running jobs
-		logger.info("Caught interrupt, destroying all running jobs")
-		job_server.destroy()
-		result.get_remaining_stats()
-		
-	# Wait for all stats jobs to finish
-	job_server.wait(group="stats")	
-	
-	while len(result.stats.keys()) < len(result.motifs):
-		sleep(10)
-		logger.info("Waiting, this should not happen...")
-	
-	return result
+    if not job_server:
+        job_server = pp.Server(ncpus, secret='pumpkinrisotto')
+    
+    jobs = {}
+    
+    result = PredictionResult(outfile, logger=logger, fg_file=fg_file, bg_file=bg_file, job_server=job_server)
+    
+    # Dynamically load all tools
+    toolio = [x[1]() for x in inspect.getmembers(tool_classes, lambda x: inspect.isclass(x) and issubclass(x, tool_classes.MotifProgram)) if x[0] != 'MotifProgram']
+    
+    # TODO:
+    # Add warnings for running time: Weeder, MoAn, GADEM
+        
+    ### Add all jobs to the job_server ###
+    params = {'analysis': analysis, 'background':background, "single":single, "organism":organism}
+    for t in toolio:
+        if tools.has_key(t.name) and tools[t.name]:
+            if t.use_width:
+                for i in range(wmin, wmax + 1, step):
+                    logger.debug("Starting %s job, width %s" % (t.name, i))
+                    job_name = "%s_width_%s" % (t.name, i)
+                    params['width'] = i
+                    jobs[job_name] = job_server.submit(
+                        t.run, 
+                        (fastafile, ".", params,), 
+                        (tool_classes.MotifProgram,),
+                        ("gimmemotifs.config",),  
+                        result.add_motifs, 
+                        (job_name,))
+            else:
+                logger.debug("Starting %s job" % t.name)
+                job_name = t.name
+                jobs[job_name] = job_server.submit(
+                    t.run, 
+                    (fastafile, ".", params), 
+                    (tool_classes.MotifProgram,),
+                    ("gimmemotifs.config",), 
+                    result.add_motifs, 
+                    (job_name,))
+        else:
+            logger.debug("Skipping %s" % t.name)
+    
+    ### Wait until all jobs are finished or the time runs out ###
+    start_time = time()    
+    try:
+        # Run until all jobs are finished
+        while len(result.finished) < len(jobs.keys()) and (not(max_time) or time() - start_time < max_time):
+            pass
+        if len(result.finished) < len(jobs.keys()):
+            logger.info("Maximum allowed running time reached, destroying remaining jobs")
+            job_server.destroy()
+            result.get_remaining_stats()
+    ### Or the user gets impatient... ###
+    except KeyboardInterrupt, e:
+        # Destroy all running jobs
+        logger.info("Caught interrupt, destroying all running jobs")
+        job_server.destroy()
+        result.get_remaining_stats()
+        
+    # Wait for all stats jobs to finish
+    job_server.wait(group="stats")    
+    
+    logger.info("Waiting for calculation of motif statistics to finish")
+    while len(result.stats.keys()) < len(result.motifs):
+        sleep(5)
+    
+    return result
--- gimmemotifs/rocmetrics.py
+++ gimmemotifs/rocmetrics.py
@@ -12,209 +12,210 @@ from scipy.stats import stats
 from numpy import array
 
 def fraction_fdr(fg_vals, bg_vals, fdr=5):
-	from scipy.stats import scoreatpercentile
-	fg_vals = array(fg_vals)
-	s = scoreatpercentile(bg_vals, 100 - fdr)
-	return len(fg_vals[fg_vals >= s]) / float(len(fg_vals))
+    from scipy.stats import scoreatpercentile
+    fg_vals = array(fg_vals)
+    s = scoreatpercentile(bg_vals, 100 - fdr)
+    return len(fg_vals[fg_vals >= s]) / float(len(fg_vals))
 
 def score_at_fdr(fg_vals, bg_vals, fdr=5):
-	from scipy.stats import scoreatpercentile
-	bg_vals = array(bg_vals)
-	return scoreatpercentile(bg_vals, 100 - fdr)
+    from scipy.stats import scoreatpercentile
+    bg_vals = array(bg_vals)
+    return scoreatpercentile(bg_vals, 100 - fdr)
 
 def enr_at_fdr(fg_vals, bg_vals, fdr=5):
-	from scipy.stats import scoreatpercentile
-	pos = array(fg_vals)
-	neg = array(bg_vals)
-	s = scoreatpercentile(neg, 100 - fdr)
-	return len(pos[pos >= s]) / float(len(neg[neg >= s])) * len(neg) / float(len(pos))
+    from scipy.stats import scoreatpercentile
+    pos = array(fg_vals)
+    neg = array(bg_vals)
+    s = scoreatpercentile(neg, 100 - fdr)
+    return len(pos[pos >= s]) / float(len(neg[neg >= s])) * len(neg) / float(len(pos))
 
 def max_enrichment(fg_vals, bg_vals):
-	from numpy import array,hstack
-	pos = array(fg_vals)
-	neg = array(bg_vals)
+    from numpy import array,hstack
+    pos = array(fg_vals)
+    neg = array(bg_vals)
 
-	factor = len(neg) / float(len(pos))
-	scores = array([s for s in hstack((pos, neg)) if sum(neg >= s) > 2])
-	enr = array([(sum(pos >= x) / float(sum(neg >= x))) * factor for x in scores])
-	return max(enr), scores[enr.argmax()]
+    factor = len(neg) / float(len(pos))
+    scores = array([s for s in hstack((pos, neg)) if sum(neg >= s) > 2])
+    enr = array([(sum(pos >= x) / float(sum(neg >= x))) * factor for x in scores])
+    return max(enr), scores[enr.argmax()]
 
 def MNCP(fg_vals, bg_vals):
-	from scipy.stats import stats
-	from numpy import mean,array,hstack
-	#from pylab import *
-	fg_len = len(fg_vals)
-	total_len = len(fg_vals) + len(bg_vals)
+    from scipy.stats import stats
+    from numpy import mean,array,hstack
+    #from pylab import *
+    fg_len = len(fg_vals)
+    total_len = len(fg_vals) + len(bg_vals)
 
-	if type(fg_vals) != type(array([])):
-		fg_vals = array(fg_vals)
-	if type(bg_vals) != type(array([])):
-		bg_vals = array(bg_vals)
-	
-	fg_rank = stats.rankdata(fg_vals)
-	total_rank = stats.rankdata(hstack((fg_vals, bg_vals)))
+    if type(fg_vals) != type(array([])):
+        fg_vals = array(fg_vals)
+    if type(bg_vals) != type(array([])):
+        bg_vals = array(bg_vals)
+    
+    fg_rank = stats.rankdata(fg_vals)
+    total_rank = stats.rankdata(hstack((fg_vals, bg_vals)))
 
-	slopes = []
-	for i in range(len(fg_vals)):
-		slope = ((fg_len - fg_rank[i] + 1) / fg_len ) / ((total_len - total_rank[i] + 1)/ total_len)
-		slopes.append(slope)
-	return mean(slopes)
+    slopes = []
+    for i in range(len(fg_vals)):
+        slope = ((fg_len - fg_rank[i] + 1) / fg_len ) / ((total_len - total_rank[i] + 1)/ total_len)
+        slopes.append(slope)
+    return mean(slopes)
 
 def ROC_AUC(fg_vals, bg_vals):
-	from scipy.stats import stats
-	from numpy import mean,array,hstack
-	#if len(fg_vals) != len(bg_vals):
-	#	return None
-	
-	if len(fg_vals) == 0 or len(bg_vals) == 0:
-		return None
-	
-	fg_len = len(fg_vals)
-	total_len = len(fg_vals) + len(bg_vals)
-	
-	if type(fg_vals) != type(array([])):
-		fg_vals = array(fg_vals)
-	if type(bg_vals) != type(array([])):
-		bg_vals = array(bg_vals)
+    from scipy.stats import stats
+    from numpy import mean,array,hstack
+    #if len(fg_vals) != len(bg_vals):
+    #    return None
+    
+    if len(fg_vals) == 0 or len(bg_vals) == 0:
+        return None
+    
+    fg_len = len(fg_vals)
+    total_len = len(fg_vals) + len(bg_vals)
+    
+    if type(fg_vals) != type(array([])):
+        fg_vals = array(fg_vals)
+    if type(bg_vals) != type(array([])):
+        bg_vals = array(bg_vals)
 
-	fg_rank = stats.rankdata(fg_vals) 
-	total_rank = stats.rankdata(hstack((fg_vals, bg_vals)))
-	
-	return (sum(total_rank[:fg_len]) - sum(fg_rank))/ (fg_len * (total_len - fg_len))
+    fg_rank = stats.rankdata(fg_vals) 
+    total_rank = stats.rankdata(hstack((fg_vals, bg_vals)))
+    
+    return (sum(total_rank[:fg_len]) - sum(fg_rank))/ (fg_len * (total_len - fg_len))
 
 def ROC_AUC_xlim(x_bla, y_bla, xlim=None):
-	x = x_bla[:]
-	y = y_bla[:]
+    x = x_bla[:]
+    y = y_bla[:]
 
-	x.sort()
-	y.sort()
+    x.sort()
+    y.sort()
 
-	u = {}
-	for i in x + y:
-		u[i] = 1
+    u = {}
+    for i in x + y:
+        u[i] = 1
 
-	vals = u.keys()
-	vals.sort()
-	
-	len_x = float(len(x))
-	len_y = float(len(y))
-	
-	new_x = []
-	new_y = []
-	
-	x_p = 0
-	y_p = 0
-	for val in vals[::-1]:
-		while len(x) > 0 and x[-1] >= val:
-			x.pop()
-			x_p += 1
-		while len(y) > 0 and y[-1] >= val:
-			y.pop()
-			y_p += 1
-		new_y.append((len_x - x_p) / len_x)
-		new_x.append((len_y - y_p) / len_y)
-	
-	#print new_x
-	#print new_y
-	new_x = 1 - array(new_x)
-	new_y = 1 - array(new_y)
-	#plot(new_x, new_y)
-	#show()
+    vals = u.keys()
+    vals.sort()
+    
+    len_x = float(len(x))
+    len_y = float(len(y))
+    
+    new_x = []
+    new_y = []
+    
+    x_p = 0
+    y_p = 0
+    for val in vals[::-1]:
+        while len(x) > 0 and x[-1] >= val:
+            x.pop()
+            x_p += 1
+        while len(y) > 0 and y[-1] >= val:
+            y.pop()
+            y_p += 1
+        new_y.append((len_x - x_p) / len_x)
+        new_x.append((len_y - y_p) / len_y)
+    
+    #print new_x
+    #print new_y
+    new_x = 1 - array(new_x)
+    new_y = 1 - array(new_y)
+    #plot(new_x, new_y)
+    #show()
 
-	x = new_x
-	y = new_y
+    x = new_x
+    y = new_y
 
-	if len(x) != len(y):
-		raise "Unequal!"
+    if len(x) != len(y):
+        raise "Unequal!"
 
-	if not xlim:
-		xlim = 1.0
+    if not xlim:
+        xlim = 1.0
 
-	auc = 0.0
-	bla = zip(stats.rankdata(x), range(len(x)))
+    auc = 0.0
+    bla = zip(stats.rankdata(x), range(len(x)))
 
-	def sortfunc(x,y):
-		res = x[0] - y[0]
-		if res < 0:
-			return -1
-		elif res > 0:
-			return 1
-		elif res == 0:
-			return y[1] - x[1]
-	
-	bla.sort(sortfunc)
-	
-	prev_x = x[bla[0][1]]
-	prev_y = y[bla[0][1]]
-	index = 1
+    def sortfunc(x,y):
+        res = x[0] - y[0]
+        if res < 0:
+            return -1
+        elif res > 0:
+            return 1
+        elif res == 0:
+            return y[1] - x[1]
+    
+    bla.sort(sortfunc)
+    
+    prev_x = x[bla[0][1]]
+    prev_y = y[bla[0][1]]
+    index = 1
 
-	while index < len(bla) and x[bla[index][1]] <= xlim:
+    while index < len(bla) and x[bla[index][1]] <= xlim:
 
-		(rank, i) = bla[index]
-		
-		auc += y[i] * (x[i] - prev_x) - ((x[i] - prev_x) * (y[i] - prev_y) / 2.0)
-		prev_x = x[i]
-		prev_y = y[i]
-		index += 1
-	
-	if index < len(bla):
-		(rank, i) = bla[index]
-		auc += prev_y * (xlim - prev_x) + ((y[i] - prev_y)/(x[i] - prev_x) * (xlim -prev_x) * (xlim - prev_x)/2)
+        (rank, i) = bla[index]
+        
+        auc += y[i] * (x[i] - prev_x) - ((x[i] - prev_x) * (y[i] - prev_y) / 2.0)
+        prev_x = x[i]
+        prev_y = y[i]
+        index += 1
+    
+    if index < len(bla):
+        (rank, i) = bla[index]
+        auc += prev_y * (xlim - prev_x) + ((y[i] - prev_y)/(x[i] - prev_x) * (xlim -prev_x) * (xlim - prev_x)/2)
 
-	return auc
+    return auc
 
 def ROC_values(x_bla, y_bla):
-	if len(x_bla) == 0 or len(y_bla) == 0:
-		return [],[]
+    if len(x_bla) == 0 or len(y_bla) == 0:
+        return [],[]
 
-	x = x_bla[:]
-	y = y_bla[:]
+    x = x_bla[:]
+    y = y_bla[:]
 
-	x.sort()
-	y.sort()
+    x.sort()
+    y.sort()
 
-	u = {}
-	for i in x + y:
-		u[i] = 1
+    u = {}
+    for i in x + y:
+        u[i] = 1
 
-	vals = u.keys()
-	vals.sort()
-	
-	len_x = float(len(x))
-	len_y = float(len(y))
-	
-	new_x = []
-	new_y = []
-	
-	x_p = 0
-	y_p = 0
-	for val in vals[::-1]:
-		while len(x) > 0 and x[-1] >= val:
-			x.pop()
-			x_p += 1
-		while len(y) > 0 and y[-1] >= val:
-			y.pop()
-			y_p += 1
-		new_y.append((len_x - x_p) / len_x)
-		new_x.append((len_y - y_p) / len_y)
-	
-	#print new_x
-	#print new_y
-	new_x = 1 - array(new_x)
-	new_y = 1 - array(new_y)
-	
-	return (new_x, new_y)
+    vals = u.keys()
+    vals.sort()
+    
+    len_x = float(len(x))
+    len_y = float(len(y))
+    
+    new_x = []
+    new_y = []
+    
+    x_p = 0
+    y_p = 0
+    for val in vals[::-1]:
+        while len(x) > 0 and x[-1] >= val:
+            x.pop()
+            x_p += 1
+        while len(y) > 0 and y[-1] >= val:
+            y.pop()
+            y_p += 1
+        new_y.append((len_x - x_p) / len_x)
+        new_x.append((len_y - y_p) / len_y)
+    
+    #print new_x
+    #print new_y
+    new_x = 1 - array(new_x)
+    new_y = 1 - array(new_y)
+    
+    return (new_x, new_y)
 
 def max_fmeasure(x,y):
-	x = array(x[:])
-	y = array(y[:])
-	p = y / (y + x)
-	filt = logical_and((p * y) > 0, (p + y) > 0)
-	p = p[filt]
-	y = y[filt]
-	
-	f = (2 * p * y) / (p + y)
-	if len(f) > 0:
-		return nanmax(f), nanmax(y[f == nanmax(f)])
-	else:
-		return None,None
+    from numpy import array, logical_and, nanmax
+    x = array(x[:])
+    y = array(y[:])
+    p = y / (y + x)
+    filt = logical_and((p * y) > 0, (p + y) > 0)
+    p = p[filt]
+    y = y[filt]
+    
+    f = (2 * p * y) / (p + y)
+    if len(f) > 0:
+        return nanmax(f), nanmax(y[f == nanmax(f)])
+    else:
+        return None,None
