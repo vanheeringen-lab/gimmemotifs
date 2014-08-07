5a2a1a1ee66ad792fd48d8ea709792610d07fe43
Simon van Heeringen <simon.vanheeringen@gmail.com>
2014-08-07 15:12:36 +0200

    Updated to 'scan' and 'diff'

--- gimmemotifs/commands/diff.py
+++ gimmemotifs/commands/diff.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
-# Copyright (c) 2013 Simon van Heeringen <s.vanheeringen@ncmls.ru.nl>
+# Copyright (c) 2013-2014 Simon van Heeringen <s.vanheeringen@ncmls.ru.nl>
 #
 # This module is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License, see the file COPYING included with this 
 # distribution.
 import sys
 import os
+import shutil
 import numpy as np
+from gimmemotifs.config import MotifConfig
+from gimmemotifs.genome_index import *
 from gimmemotifs.scan import get_counts
 from gimmemotifs.motif import pwmfile_to_motifs
 from gimmemotifs.fasta import Fasta
 from gimmemotifs.plot import diff_plot
+from tempfile import mkdtemp
 
 def diff(args):
 
@@ -19,6 +23,36 @@ def diff(args):
     outfile = args.outputfile
     pwmfile = args.pwmfile
     cutoff = args.cutoff
+    genome = args.genome
+    minenr = float(args.minenr)
+
+    tmpdir = mkdtemp()
+    
+    # Retrieve FASTA clusters from BED file
+    if len(infiles) == 1 and infiles[0].endswith("bed"):
+        if not args.genome:
+            sys.stderr.write("Can't convert BED file without genome!\n")
+            sys.exit(1)
+
+        clusters = {}
+        for line in open(infiles[0]):
+            vals = line.strip().split("\t")
+            clusters.setdefault(vals[3], []).append(vals[:3])
+        
+        infiles = []
+        
+        config = MotifConfig()
+        index_dir = config.get_index_dir()
+
+        for cluster,regions in clusters.items():
+            sys.stderr.write("Creating FASTA file for {0}\n".format(cluster))
+            inbed = os.path.join(tmpdir, "{0}.bed".format(cluster))
+            outfa = os.path.join(tmpdir, "{0}.fa".format(cluster))
+            with open(inbed, "w") as f:
+                for vals in regions:
+                    f.write("{0}\t{1}\t{2}\n".format(*vals))
+            track2fasta(os.path.join(index_dir, genome), inbed, outfa)
+            infiles.append(outfa)
     
     pwms = dict([(m.id, m) for m in pwmfile_to_motifs(pwmfile)])
     motifs = [m for m in pwms.keys()]
@@ -44,4 +78,6 @@ def diff(args):
     #for row in freq:
     #    print freq
 
-    diff_plot(motifs, pwms, names, freq, counts, bgfreq, bgcounts, outfile)
+    diff_plot(motifs, pwms, names, freq, counts, bgfreq, bgcounts, outfile, minenr=minenr)
+
+    shutil.rmtree(tmpdir)
--- gimmemotifs/commands/pwmscan.py
+++ gimmemotifs/commands/pwmscan.py
@@ -27,30 +27,47 @@ def pwmscan(args):
    
     p = re.compile(r'([^\s:]+):(\d+)-(\d+)')
     fa = Fasta(inputfile)
-    strandmap = {-1:"-",1:"+"}
-    for motif, result in result:
-        for seq_id, matches in result.items():
-            for (pos, score, strand) in matches:
-                if bed:
-                    m = p.search(seq_id)
-                    if m:
-                        chrom = m.group(1)
-                        start = int(m.group(2))
-                        end = int(m.group(3))
-                        print "%s\t%s\t%s\t%s\t%s\t%s" % (chrom, start + pos, start + pos + len(motif) , motif.id, score, strandmap[strand])
-                    else:
-                        print "%s\t%s\t%s\t%s\t%s\t%s" % (seq_id, pos, pos +  len(motif), motif.id, score, strandmap[strand])
-                else:
-                    print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tmotif_name \"%s\" ; motif_instance \"%s\"" % (
-                        seq_id, 
-                        "pwmscan", 
-                        "misc_feature", 
-                        pos + 1,            # GFF is 1-based
-                        pos + len(motif), 
-                        score, 
-                        strandmap[strand], 
-                        ".", 
-                        motif.id, 
-                        fa[seq_id][pos: pos + len(motif)]
-                    )
+    if args.table:
+        table = {}
+        for seq_id in fa.ids:
+            table[seq_id] = {}
 
+        for motif, result in result:
+            for seq_id, matches in result.items():
+                table[seq_id][motif] = len(matches)
+        
+        #mnames = [m.id for m in motifs]
+        #print table
+        print "\t{}".format("\t".join([m.id for m in motifs]))
+        for seq_id in fa.ids:
+            counts = [table[seq_id].get(m, 0) for m in motifs]
+            print "{}\t{}".format(seq_id, "\t".join([str(x) for x in counts]))
+
+    else:
+        strandmap = {-1:"-",1:"+"}
+        for motif, result in result:
+            for seq_id, matches in result.items():
+                for (pos, score, strand) in matches:
+                    if bed:
+                        m = p.search(seq_id)
+                        if m:
+                            chrom = m.group(1)
+                            start = int(m.group(2))
+                            end = int(m.group(3))
+                            print "%s\t%s\t%s\t%s\t%s\t%s" % (chrom, start + pos, start + pos + len(motif) , motif.id, score, strandmap[strand])
+                        else:
+                            print "%s\t%s\t%s\t%s\t%s\t%s" % (seq_id, pos, pos +  len(motif), motif.id, score, strandmap[strand])
+                    else:
+                        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tmotif_name \"%s\" ; motif_instance \"%s\"" % (
+                            seq_id, 
+                            "pwmscan", 
+                            "misc_feature", 
+                            pos + 1,            # GFF is 1-based
+                            pos + len(motif), 
+                            score, 
+                            strandmap[strand], 
+                            ".", 
+                            motif.id, 
+                            fa[seq_id][pos: pos + len(motif)]
+                        )
+    
