1cbe1b8bd33a73d3303f41d313c51ad733d5c0c6
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-03-17 10:08:27 +0100

    enable cache in config

--- gimmemotifs/scanner.py
+++ gimmemotifs/scanner.py
@@ -14,20 +14,6 @@ from dogpile.cache.api import NO_VALUE
 
 from cityhash import CityHash64
 
-cache = make_region().configure(
-    'dogpile.cache.pylibmc',
-    expiration_time = 3600,
-    arguments = {
-        'url':["127.0.0.1"],
-        'binary': True,
-    }
-#    'dogpile.cache.dbm',
-#    expiration_time = 3600,
-#    arguments = {
-#        'filename': 'cache.dbm'
-#    }
-)
-
 def load_motifs(motif_file, cutoff=0.95):
     motifs = read_motifs(open(motif_file))
     d = parse_cutoff(motifs, cutoff)
@@ -57,58 +43,6 @@ def scan_region(region, genome_index, motifs, nreport, scan_rc):
     
     return scan_sequence(seq, motifs, nreport, scan_rc)
 
-def scan_regions(regions, index_dir, motif_file, nreport, scan_rc, cutoff=0.95, use_cache=False):
-    
-    # determine which regions are not in the cache 
-    scan_regions = regions
-    if use_cache:
-        scan_regions = []
-        for region in regions:
-            key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
-            ret = cache.get(key)
-            if ret == NO_VALUE:
-                scan_regions.append(region)
-    
-    # scan the regions that are not in the cache
-    if len(scan_regions) > 0:
-        n = 12
-        
-        genome_index = GenomeIndex(index_dir)
-       
-        motifs = load_motifs(motif_file, cutoff)
- 
-        scan_func = partial(scan_region_mult,
-            genome_index=genome_index,
-            motifs=motifs,
-            nreport=nreport,
-            scan_rc=scan_rc)
-
-        jobs = []
-        chunksize = len(scan_regions) / n + 1
-        for i in range(n):
-            job = pool.apply_async(scan_func, (scan_regions[i * chunksize:( i+ 1) * chunksize],))
-            jobs.append(job)
-        
-        # store values in cache
-        i = 0
-        for job in jobs:
-            for ret in job.get():
-                if use_cache:
-                    # store values in cache    
-                    region = scan_regions[i]
-                    key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
-                    cache.set(key, ret)
-                else:
-                    yield ret
-                i += 1
-
-    if use_cache: 
-        # return results from cache
-        for region in regions:
-            key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
-            ret = cache.get(key)
-            yield ret
-
 def scan_seq_mult(seqs, motifs, nreport, scan_rc):
     ret = []
     for seq in seqs:
@@ -123,54 +57,6 @@ def scan_region_mult(regions, genome_index, motifs, nreport, scan_rc):
         ret.append(result)
     return ret
 
-def scan_sequences(seqs, motif_file, nreport, scan_rc, cutoff=0.95, use_cache=False):
-    
-    scan_seqs = seqs
-    if use_cache:
-        # determine which sequences are not in the cache 
-        hashes = dict([(s.upper(), CityHash64(s.upper())) for s in seqs])
-        scan_seqs = []
-    
-        for seq,seq_hash in hashes.items():
-            key = str((seq_hash, motif_file, nreport, scan_rc, cutoff))
-            ret = cache.get(key)
-            if ret == NO_VALUE:
-                scan_seqs.append(seq.upper())
-    
-    # scan the sequences that are not in the cache
-    if len(scan_seqs) > 0:
-        n = 12
-        motifs = load_motifs(motif_file, cutoff)
-        scan_func = partial(scan_seq_mult,
-            motifs=motifs,
-            nreport=nreport,
-            scan_rc=scan_rc)
-
-        jobs = []
-        chunksize = len(scan_seqs) / n + 1 
-        for i in range(n):
-            job = pool.apply_async(scan_func, (scan_seqs[i * chunksize:(i  + 1) * chunksize],))
-            jobs.append(job)
-        
-        # store values in cache
-        i = 0
-        for job in jobs:
-            for ret in job.get():
-                if use_cache:
-                    h = hashes[scan_seqs[i]]
-                    key = str((h, motif_file, nreport, scan_rc, cutoff))
-                    cache.set(key, ret)
-                else: 
-                    yield ret
-                i += 1
-    
-    if use_cache:
-        # return results from cache
-        for seq in seqs:
-            key = str((hashes[seq.upper()], motif_file, nreport, scan_rc, cutoff))
-            ret = cache.get(key)
-            yield ret
-
 class Scanner(object):
     """
     scan sequences with motifs
@@ -178,6 +64,29 @@ class Scanner(object):
     
     def __init__(self):
         self.config = MotifConfig()
+
+        self.use_cache = False
+        if self.config.get_default_params().get("use_cache", False):
+            self._init_cache()
+            
+    def _init_cache(self):
+        try:
+            self.cache = make_region().configure(
+                'dogpile.cache.pylibmc',
+                expiration_time = 3600,
+                arguments = {
+                    'url':["127.0.0.1"],
+                    'binary': True,
+                    }
+                #    'dogpile.cache.dbm',
+                #    expiration_time = 3600,
+                #    arguments = {
+                #        'filename': 'cache.dbm'
+                #    }
+            )
+            self.use_cache = True
+        except:
+            sys.stderr.write("failed to initialize cache\n")
 
     def set_motifs(self, motifs):
         self.motifs = motifs
@@ -291,17 +200,122 @@ class Scanner(object):
             if seqs_type.endswith("file"):
                 seqs = Fasta(seqs)
             
-            it = scan_sequences(seqs.seqs, 
-                    self.motifs, nreport, scan_rc, cutoff, use_cache=True)
+            it = self._scan_sequences(seqs.seqs, 
+                    nreport, scan_rc, cutoff)
         # regions or BED
         else:
             if seqs_type == "regionfile":
                 seqs = [l.strip() for l in open(seqs)]
-            it = scan_regions(seqs, self.index_dir,
-                    self.motifs, nreport, scan_rc, cutoff, use_cache=True)
+            it = self._scan_regions(seqs, 
+                    nreport, scan_rc, cutoff)
         
         for result in it:
             yield result
+
+
+    def _scan_regions(self, regions, nreport, scan_rc, cutoff=0.95):
+        index_dir = self.index_dir
+        motif_file = self.motifs
+
+        # determine which regions are not in the cache 
+        scan_regions = regions
+        if self.use_cache:
+            scan_regions = []
+            for region in regions:
+                key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
+                ret = self.cache.get(key)
+                if ret == NO_VALUE:
+                    scan_regions.append(region)
+        
+        # scan the regions that are not in the cache
+        if len(scan_regions) > 0:
+            n = 12
+            
+            genome_index = GenomeIndex(index_dir)
+           
+            motifs = load_motifs(motif_file, cutoff)
+     
+            scan_func = partial(scan_region_mult,
+                genome_index=genome_index,
+                motifs=motifs,
+                nreport=nreport,
+                scan_rc=scan_rc)
+    
+            jobs = []
+            chunksize = len(scan_regions) / n + 1
+            for i in range(n):
+                job = pool.apply_async(scan_func, (scan_regions[i * chunksize:( i+ 1) * chunksize],))
+                jobs.append(job)
+            
+            # store values in cache
+            i = 0
+            for job in jobs:
+                for ret in job.get():
+                    if self.use_cache:
+                        # store values in cache    
+                        region = scan_regions[i]
+                        key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
+                        self.cache.set(key, ret)
+                    else:
+                        yield ret
+                    i += 1
+    
+        if self.use_cache: 
+            # return results from cache
+            for region in regions:
+                key = str((region, index_dir, motif_file, nreport, scan_rc, cutoff))
+                ret = self.cache.get(key)
+                yield ret
+    
+    def _scan_sequences(self, seqs, nreport, scan_rc, cutoff=0.95):
+        
+        motif_file = self.motifs
+        
+        scan_seqs = seqs
+        if self.use_cache:
+            # determine which sequences are not in the cache 
+            hashes = dict([(s.upper(), CityHash64(s.upper())) for s in seqs])
+            scan_seqs = []
+        
+            for seq,seq_hash in hashes.items():
+                key = str((seq_hash, motif_file, nreport, scan_rc, cutoff))
+                ret = self.cache.get(key)
+                if ret == NO_VALUE:
+                    scan_seqs.append(seq.upper())
+        
+        # scan the sequences that are not in the cache
+        if len(scan_seqs) > 0:
+            n = 12
+            motifs = load_motifs(motif_file, cutoff)
+            scan_func = partial(scan_seq_mult,
+                motifs=motifs,
+                nreport=nreport,
+                scan_rc=scan_rc)
+    
+            jobs = []
+            chunksize = len(scan_seqs) / n + 1 
+            for i in range(n):
+                job = pool.apply_async(scan_func, (scan_seqs[i * chunksize:(i  + 1) * chunksize],))
+                jobs.append(job)
+            
+            # store values in cache
+            i = 0
+            for job in jobs:
+                for ret in job.get():
+                    if self.use_cache:
+                        h = hashes[scan_seqs[i]]
+                        key = str((h, motif_file, nreport, scan_rc, cutoff))
+                        self.cache.set(key, ret)
+                    else: 
+                        yield ret
+                    i += 1
+        
+        if self.use_cache:
+            # return results from cache
+            for seq in seqs:
+                key = str((hashes[seq.upper()], motif_file, nreport, scan_rc, cutoff))
+                ret = self.cache.get(key)
+                yield ret
 
 try: 
     from gimmemotifs.mp import pool
