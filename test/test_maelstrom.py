230f1d5857b1bdcd28bb284fa6dfa92691a29ea6
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-06-07 14:07:35 +0200

    updated test

--- test/test_maelstrom.py
+++ test/test_maelstrom.py
@@ -2,6 +2,8 @@ import unittest
 import tempfile
 import os
 from gimmemotifs.maelstrom import run_maelstrom
+import pandas as pd
+from glob import glob
 
 class TestMoap(unittest.TestCase):
     """ A test class to test maelstrom"""
@@ -9,8 +11,16 @@ class TestMoap(unittest.TestCase):
     def setUp(self):
         self.outdir = "test/data/maelstrom"
         self.clusters = "test/data/moap/clusters.txt"
+        self.outfile = os.path.join(self.outdir, "final.out.csv")
 
     def test1_maelstrom(self):
         """ Test Motif Activity by Ensemble Learning (maelstrom) """
         
         run_maelstrom(self.clusters, "mm10", self.outdir)
+        df = pd.read_table(self.outfile, index_col=0)
+        self.assertEquals((623, 4), df.shape)
+
+        for fname in glob(os.path.join(self.outdir, "changed*")):
+            os.unlink(fname)
+        os.unlink(self.outfile)
+
