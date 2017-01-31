336ebdb2b914a3a6d77ed5df475de7d41f0741d7
Simon van Heeringen <simon.vanheeringen@gmail.com>
2017-01-31 08:24:03 +0100

    style

--- gimmemotifs/commands/maelstrom.py
+++ gimmemotifs/commands/maelstrom.py
@@ -4,12 +4,13 @@
 # This module is free software. You can redistribute it and/or modify it under 
 # the terms of the MIT License, see the file COPYING included with this 
 # distribution.
+"""Command line function 'maelstrom'"""
 import os
-import sys
 from gimmemotifs.genome_index import check_genome
 from gimmemotifs.maelstrom import run_maelstrom
 
 def maelstrom(args):
+    """Run the maelstrom method."""
     infile = args.inputfile
     genome = args.genome
     outdir = args.outdir
@@ -18,6 +19,7 @@ def maelstrom(args):
     if not os.path.exists(infile):
         raise ValueError("file {} does not exist".format(infile))
 
+    # check if the genome exists and is indexed
     check_genome(genome)
                 
     run_maelstrom(infile, genome, outdir, pwmfile)
