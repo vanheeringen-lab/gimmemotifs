5dd6503719e1d8e0e56da8f7e212c3413166f05a
Simon van Heeringen <simon.vanheeringen@gmail.com>
2017-06-30 16:34:35 +0200

    version bump

--- gimmemotifs/config.py
+++ gimmemotifs/config.py
@@ -13,7 +13,7 @@ import logging
 logger = logging.getLogger("gimme.config")
 
 ### CONSTANTS ###
-GM_VERSION = "0.10.1b2"
+GM_VERSION = "0.11.0-beta"
 BG_TYPES = ["random", "genomic", "gc", "promoter"]
 FA_VALID_BGS = ["random", "promoter", "gc", "user", "genomic"]
 BED_VALID_BGS = ["random", "genomic", "gc", "promoter", "user"]
