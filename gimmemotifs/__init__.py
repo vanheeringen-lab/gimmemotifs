23b3d561fb57f3d0f04595d206708ab988f0dbbf
Simon van Heeringen <simon.vanheeringen@gmail.com>
2019-04-15 16:48:46 +0200

    filter similar messages

--- gimmemotifs/__init__.py
+++ gimmemotifs/__init__.py
@@ -10,6 +10,16 @@ def mytmpdir():
         atexit.register(shutil.rmtree, mytmpdir.dir)
     return mytmpdir.dir
 
+class DuplicateFilter(logging.Filter):
+    # https://stackoverflow.com/questions/44691558
+    def filter(self, record):
+        # add other fields if you need more granular comparison, depends on your app
+        current_log = (record.module, record.levelno, record.msg)
+        if current_log != getattr(self, "last_log", None):
+            self.last_log = current_log
+            return True
+        return False
+
 logger = logging.getLogger('gimme')
 logger.setLevel(logging.DEBUG)
 logger.propagate = 0
@@ -22,6 +32,7 @@ sh = logging.StreamHandler()
 sh.setLevel(logging.INFO)
 sh.setFormatter(screen_formatter)
 logger.addHandler(sh)
+logger.addFilter(DuplicateFilter())
 
 from ._version import get_versions
 __version__ = get_versions()['version']
