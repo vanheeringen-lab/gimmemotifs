98ec2776148871e58d81afe4f6a7b3dbcba3e305
simonvh <simon.vanheeringen@gmail.com>
2019-10-22 19:44:49 +0200

    added cli tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -4,6 +4,7 @@ from tempfile import TemporaryDirectory
 import pytest
 
 from gimmemotifs.cli import cli
+from gimmemotifs.motif import read_motifs
 
 travis = "TRAVIS" in os.environ and os.environ["TRAVIS"] == "true"
 
@@ -56,12 +57,35 @@ def test_gimme_scan(arguments):
 
     assert 1 == 1
 
+
 def test_gimme_logo():
     motif_name = "MA0103.3_ZEB1"
-    cli(
-        ["logo", "test/data/cli/motifs.pfm", "-i", motif_name]
-    )
+    cli(["logo", "test/data/cli/motifs.pfm", "-i", motif_name])
 
     assert os.path.exists(f"{motif_name}.png")
     if os.path.exists(f"{motif_name}.png"):
         os.unlink(f"{motif_name}.png")
+
+
+def test_gimme_match(capsys):
+    pfm = "test/data/cli/test_motif.pfm"
+    for db_args, out in [
+        ([], "GM.5.0.p53.0005"),
+        (["-d", "JASPAR2020_vertebrates"], "MA0106.3_TP53"),
+    ]:
+
+        cli(["match", pfm] + db_args)
+        captured = capsys.readouterr()
+        match = captured.out.strip().split("\n")[-1].split("\t")[1]
+        assert match == out
+
+
+def test_gimme_cluster():
+    pfm = "test/data/cli/cluster.pfm"
+
+    with TemporaryDirectory() as d:
+        cli(["cluster", pfm, d])
+        out_pfm = os.path.join(d, "clustered_motifs.pwm")
+        assert os.path.exists(out_pfm)
+        cons = ["ACCGTTAACsGy", "ATGACkyA", "TTGCGnAA"]
+        assert cons == sorted([m.to_consensus() for m in read_motifs(out_pfm)])
