5489cff1e3bc93f9a2ffb6e65b31a94c68621096
simonvh <simon.vanheeringen@gmail.com>
2019-11-20 20:08:53 +0100

    style

--- test/_test_trawler.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-from motiftools.tools import *
-t = Trawler()
-print(t._run_program("/mnt/data/gimmemotifs/chen/CTCF/CTCF_prediction.fa", params={"background":"/mnt/data/gimmemotifs/chen/CTCF/CTCF_bg_genomic_matched.fa"}))
-
