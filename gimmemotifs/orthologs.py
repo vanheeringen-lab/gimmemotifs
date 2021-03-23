520e6939a516dfdddab3ed3da658b6dca902cb05
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2021-03-23 17:42:32 +0100

    cleanup

--- gimmemotifs/orthologs.py
+++ gimmemotifs/orthologs.py
@@ -35,13 +35,16 @@ def motif2factor_from_orthologs(
     database: str = "gimme.vertebrate.v5.0",
     database_references: List[str] = ["GRCh38.p13", "GRCm38.p6"],
     extra_orthologs_references: List[str] = [
-        "danRer11",
-        "UCB_Xtro_10.0",
-        "GRCg6a",
-        "BraLan2",
-        "ASM318616v1",
-        "Astyanax_mexicanus-2.0",
-        "oryLat2",
+        "danRer11",  # zebrafish
+        "UCB_Xtro_10.0",  # xenopus
+        "GRCg6a",  # chicken
+        "BraLan2",  # lancet fish (out-group ish)
+        "ASM318616v1",  # turbot
+        "Astyanax_mexicanus-2.0",  # cave fish
+        "oryLat2",  # medaka
+        "ARS-UCD1.2",  # cow
+        "phaCin_unsw_v4.1",  # koala
+        "rCheMyd1.pri",  # turtle
     ],
     new_reference: List[str] = None,
     genomes_dir: str = None,
@@ -49,10 +52,21 @@ def motif2factor_from_orthologs(
     outdir: str = ".",
 ):
     """
+    Make a motifs2factors file based on gene ortology.
 
+    This function first downloads the genomes of the new reference, the old
+    reference (human and mouse), and a wide range of different vertebrate
+    genomes (a range of genomes is necessary to get a better ortolog inference)
+
+    Then the peptide sequence of each gene is extracted from the genome +
+    annotation, and ortologs between these are derived by orthofinder.
+
+    Finally, based on these ortologs, a new motifs2factors file is created from
+    the old one.
     """
     all_genomes = set(database_references + extra_orthologs_references + new_reference)
 
+    # TODO use tmpdir
     # TODO set outdir from args
     # outdir = tempfile.mkdtemp(dir=tmpdir)
     outdir = "/vol/gimmetest"
@@ -61,11 +75,11 @@ def motif2factor_from_orthologs(
     # download all required genomes
     _download_genomes_with_annot(all_genomes, genomes_dir)
 
-    # convert each genome + annotation into the primary transcripts (longest protein per gene)
+    # convert each genome + annotation into the primary genes (longest protein per gene)
     for genome in all_genomes:
         annot2primpep(genome, outdir)
 
-    # run ortho...
+    # run orthofinder on our primary genes
     orthofinder_result = _orthofinder(f"{outdir}/prim_genes")
 
     # now parse the output of orthofinder
@@ -91,6 +105,9 @@ def motif2factor_from_orthologs(
 
 
 def _orthofinder(peptide_folder):
+    """
+    Run orthofinder on the peptide folder
+    """
     # run orthofinder on the primary transcripts
     result = subprocess.run(
         [f"orthofinder", f"-f", peptide_folder], capture_output=True
@@ -107,8 +124,7 @@ def _orthofinder(peptide_folder):
 
 
 def _download_genomes_with_annot(genomes, genomes_dir):
-    """
-    """
+    # make sure each genome has a gene annotation
     no_annotations = [genome for genome in genomes if not _has_annotation(genome)]
     assert (
         len(no_annotations) == 0
@@ -122,11 +138,20 @@ def _download_genomes_with_annot(genomes, genomes_dir):
             ["gunzip", f"{genomes_dir}/{genome}/{genome}.annotation.gtf.gz"],
             capture_output=True,
         )
-        # TODO errors
+        # TODO error handling
 
 
 def annot2primpep(genome, outdir):
     """
+    Go from a gene annotation (gtf) to a primary proteins file. Uses gffread to
+    do the heavy lifting.
+
+    The gene identifier of the fasta consists of both the gene name and the
+    gene id, separated by a pipe symbol, like so:
+
+    >GATA4|ENSG00000136574
+    MYQSLAMAANHGPPPGAYEAGGPGAFMHGAGAASSPVYVPTPR
+    GTQQGSPGWSQAGADGAAYTPPPVSPRFSFPGTTGSLAAAAAA
     """
     # setup our result folder
     pathlib.Path(f"{outdir}/prim_genes").mkdir(parents=True, exist_ok=True)
@@ -177,6 +202,7 @@ def annot2primpep(genome, outdir):
         # add to our records
         records[prot_name] = protein
 
+    # and finally save our result
     with open(f"{outdir}/prim_genes/{genome}.pep.fa", "w") as f:
         for record, sequence in records.items():
             f.write(f">{record}\n")
@@ -184,22 +210,14 @@ def annot2primpep(genome, outdir):
             f.write(f"\n")
 
 
-def _has_annotation(genome):
-    """
-    Return True if a genome has an annotation for it, else False
-    """
-    providers = [
-        genomepy.ProviderBase.create(provider)
-        for provider in ["ensembl", "ucsc", "ncbi"]
-    ]
-    for provider in providers:
-        if genome in provider.genomes:
-            link = provider.get_annotation_download_link(genome)
-            return link is not None
-    return False
-
-
 def load_orthogroups_in_db(db, genomes, orthofinder_result):
+    """
+    Save the results of orthofinder (tsv file) in a relational database
+    (SQLite). This makes it possible to easily switch between genes and
+    ortogroups.
+
+    We create three tables; genes, ortogroups, and assemblies.
+    """
     orthogroups = pd.read_table(
         f"{orthofinder_result}/Phylogenetic_Hierarchical_Orthogroups/N0.tsv"
     )
@@ -207,49 +225,50 @@ def load_orthogroups_in_db(db, genomes, orthofinder_result):
         f"{orthofinder_result}/Orthogroups/Orthogroups_UnassignedGenes.tsv"
     )
 
+    # remove old db if it exists (TODO: shouldnt be necessary?)
     if os.path.exists(db):
         os.remove(db)
     conn = sqlite3.connect(db)
     conn.execute(
         """
-                 CREATE TABLE IF NOT EXISTS orthogroups
-                 (
-                 id integer PRIMARY KEY, 
-                 orthogroup text UNIQUE NOT NULL
-                 )
-                 """
+        CREATE TABLE IF NOT EXISTS orthogroups
+        (
+        id integer PRIMARY KEY, 
+        orthogroup text UNIQUE NOT NULL
+        )
+        """
     )
 
     conn.execute(
         """
-                 CREATE TABLE IF NOT EXISTS assemblies
-                 (
-                 id integer PRIMARY KEY, 
-                 assembly text
-                 )
-                 """
+        CREATE TABLE IF NOT EXISTS assemblies
+        (
+        id integer PRIMARY KEY, 
+        assembly text
+        )
+        """
     )
 
     conn.execute(
         """
-                 CREATE TABLE IF NOT EXISTS genes
-                 (
-                 id integer PRIMARY KEY,
-                 gene_name text,
-                 gene_name_lower text,
-                 gene_id text,
-                 gene_id_lower text,
-                 assembly NOT NULL,
-                 orthogroup,
-                 FOREIGN KEY (orthogroup) REFERENCES orthogroups (id),
-                 FOREIGN KEY (assembly) REFERENCES assemblies (assembly)
-                 )
-                 """
+        CREATE TABLE IF NOT EXISTS genes
+        (
+        id integer PRIMARY KEY,
+        gene_name text,
+        gene_name_lower text,
+        gene_id text,
+        gene_id_lower text,
+        assembly NOT NULL,
+        orthogroup,
+        FOREIGN KEY (orthogroup) REFERENCES orthogroups (id),
+        FOREIGN KEY (assembly) REFERENCES assemblies (assembly)
+        )
+        """
     )
 
-    # fill in our database
+    # fill up our database
     # all orthogroups
-    for orthogroup in orthogroups["HOG"]:
+    for orthogroup in orthogroups["HOG"]:  # <-- all Hierarchical OrthoGroups
         conn.execute(f"INSERT INTO orthogroups VALUES(NULL, '{orthogroup}')")
     conn.execute(f"INSERT INTO orthogroups VALUES(NULL, 'UNASSIGNED')")
 
@@ -266,8 +285,9 @@ def load_orthogroups_in_db(db, genomes, orthofinder_result):
                 continue
 
             for gene in genes.split(", "):
+                # for each gene we store its gene_name (if present,
+                # otherwise .) and gene_id. We load them both in the database.
                 gene_name, gene_id = gene.split("|")
-                #                 print(gene, gene_name, gene_id, gene_name != ".", orthogroup)
                 if gene_name != ".":
                     conn.execute(
                         f"INSERT INTO genes VALUES(NULL, '{gene_name}', '{gene_name.lower()}', '{gene_id}', '{gene_id.lower()}', '{assembly}', '{orthogroup}')"
@@ -283,6 +303,8 @@ def load_orthogroups_in_db(db, genomes, orthofinder_result):
             if isinstance(gene, float) and np.isnan(gene):
                 continue
 
+            # for each gene we store its gene_name (if present,
+            # otherwise .) and gene_id. We load them both in the database.
             gene_name, gene_id = gene.split("|")
             if gene_name != ".":
                 conn.execute(
@@ -296,65 +318,79 @@ def load_orthogroups_in_db(db, genomes, orthofinder_result):
     conn.commit()
     cur = conn.cursor()
 
+    # now make some indices to make it fast!
     cur.execute("CREATE INDEX idx_name_lower ON genes (gene_name_lower, assembly)")
     cur.execute("CREATE INDEX idx_id_lower ON genes (gene_id_lower, assembly)")
-    #     cur.execute("CREATE INDEX idx_gene ON genes (gene)")
-    #     cur.execute("CREATE INDEX idx_orthogroup ON orthogroups (orthogroup)")
     cur.execute("CREATE INDEX idx_orthogroup_assembly ON genes (orthogroup, assembly)")
     conn.commit()
 
     return db
 
 
-def _unknownfactor2symbols(factor, fields):
+def make_motif2factors(
+    outfile, new_reference, database_references, motifsandfactors, database
+):
     """
+    Make a motifs2factors file based on an existing ortolog database.
 
+    We loop over each motif, find which factors belong to it in the old
+    reference, then find all the orthogroups those belong to, and finally assign
+    all the genes of the new reference species that belong to those orthogroups.
+
+    However
     """
-
-    def mygeneinfo(field):
-        request = f"http://mygene.info/v3/query?q={field}:{factor}&fields=entrezgene,name,symbol,taxid,other_names"
-        try:
-            with urllib.request.urlopen(request) as url:
-                data = json.loads(url.read().decode())
-                return data["hits"]
-        except HTTPError:
-            return list()
-
-    p = multiprocessing.dummy.Pool(len(fields))
-    hits = p.map(mygeneinfo, fields)
-    hits = [item for sublist in hits for item in sublist]
-    symbols = set()
-    for hit in hits:
-        for field in ["alias", "name", "symbol"]:
-            if field in hit and not "'" in hit[field]:
-                symbols.add(hit[field])
-    return list(symbols)
-
-
-def _factor2orthogroups_sql(factor, references, database):
-    """
-
-    """
+    factor2orthogroups.cache_clear()
     conn = sqlite3.connect(database)
     cur = conn.cursor()
 
-    orthogroups = list()
-    res = cur.execute(
-        f"SELECT orthogroup FROM genes "
-        f"WHERE ("
-        + " OR ".join(f"assembly='{assembly}'" for assembly in references)
-        + ")"
-        + f"    AND (gene_name_lower='{factor.lower()}' OR gene_id_lower='{factor.lower()}')"
-    ).fetchall()
+    with open(outfile, "w") as f:
+        f.write(f"Motif\tFactor\tEvidence\tCurated\n")
+        for motif, factors in motifsandfactors.items():
+            # remember if we found any orthologs for all the factors belonging
+            # to our motif
+            motif_set = False
 
-    for orthogroup in res:
-        if orthogroup is not None:
-            orthogroups.append(*orthogroup)
-    return orthogroups
+            # get all the orthogroups belonging to all our factors belonging to
+            # our motif. This is not as trivial as it sounds, since gene naming
+            # is a mess
+            orthologousgroups = {
+                item
+                for factor in factors
+                for item in factor2orthogroups(
+                    factor, tuple(database_references), database
+                )
+            }
+            if len(orthologousgroups) > 0:
+                for orthologousgroup in orthologousgroups:
+                    res = cur.execute(
+                        f"SELECT * FROM genes WHERE orthogroup='{orthologousgroup}' AND assembly='{new_reference}'"
+                    ).fetchall()
+
+                    if len(res):
+                        for _, gene_name, _, gene_id, *_ in res:
+                            motif_set = True
+                            if gene_name is not None:
+                                f.write(f"{motif}\t{gene_name}\tOrthologs\tN\n")
+                            else:
+                                f.write(f"{motif}\t{gene_id}\tOrthologs\tN\n")
+            if not motif_set:
+                f.write(f"{motif}\tNO ORTHOLOGS FOUND\tOrthologs\tN\n")
 
 
 @lru_cache(maxsize=99999)
 def factor2orthogroups(factor, references, database):
+    """
+    Returns all the orthogroups a factor belongs to. This is NOT a trivial task.
+
+    Some of the factors shouldn't be considered (blacklisted), some have an
+    ambiguous name that should be renamed, and in general the factor naming is
+    inconsistent.
+
+    We first check whether or not it fits in our ortogroup database (easy!), if
+    not, we start querying mygeneinfo to get more gene aliases to search for in
+    our ortogroups. If that didn't work we query mygeneinfo again, but with a
+    **very** lenient query, and hope that works.
+    """
     if factor in BLACKLIST_TFS:
         return []
 
@@ -390,42 +426,72 @@ def factor2orthogroups(factor, references, database):
 
     orthogroups = list(set(orthogroups))
 
+    # TODO what to do with these cases?
     if len(orthogroups) == 0:
         print(factor)
 
     return orthogroups
 
 
-def make_motif2factors(
-    outfile, new_reference, database_references, motifsandfactors, database
-):
+def _factor2orthogroups_sql(factor, references, database):
+    """
+    Query our sql database for all orthogroups a factor belongs to
+    """
     conn = sqlite3.connect(database)
     cur = conn.cursor()
 
-    with open(outfile, "w") as f:
-        f.write(f"Motif\tFactor\tEvidence\tCurated\n")
-        for motif, factors in motifsandfactors.items():
-            motif_set = False
-            orthologousgroups = {
-                item
-                for factor in factors
-                for item in factor2orthogroups(
-                    factor, tuple(database_references), database
-                )
-            }
-            #             print(factors, orthologousgroups)
-            if len(orthologousgroups) > 0:
-                for orthologousgroup in orthologousgroups:
-                    res = cur.execute(
-                        f"SELECT * FROM genes WHERE orthogroup='{orthologousgroup}' AND assembly='{new_reference}'"
-                    ).fetchall()
+    orthogroups = list()
+    res = cur.execute(
+        f"SELECT orthogroup FROM genes "
+        f"WHERE ("
+        + " OR ".join(f"assembly='{assembly}'" for assembly in references)
+        + ")"
+        + f"    AND (gene_name_lower='{factor.lower()}' OR gene_id_lower='{factor.lower()}')"
+    ).fetchall()
 
-                    if len(res):
-                        for _, gene_name, _, gene_id, *_ in res:
-                            motif_set = True
-                            if gene_name is not None:
-                                f.write(f"{motif}\t{gene_name}\tOrthologs\tN\n")
-                            else:
-                                f.write(f"{motif}\t{gene_id}\tOrthologs\tN\n")
-            if not motif_set:
-                f.write(f"{motif}\tNO ORTHOLOGS FOUND\tOrthologs\tN\n")
+    for orthogroup in res:
+        if orthogroup is not None:
+            orthogroups.append(*orthogroup)
+    return orthogroups
+
+
+def _unknownfactor2symbols(factor, fields):
+    """
+    Query mygeneinfo for different aliases of our gene
+    """
+
+    def mygeneinfo(field):
+        request = f"http://mygene.info/v3/query?q={field}:{factor}&fields=entrezgene,name,symbol,taxid,other_names"
+        try:
+            with urllib.request.urlopen(request) as url:
+                data = json.loads(url.read().decode())
+                return data["hits"]
+        except HTTPError:
+            return list()
+
+    # multithreaded to do multiple queries at once
+    p = multiprocessing.dummy.Pool(len(fields))
+    hits = p.map(mygeneinfo, fields)
+    hits = [item for sublist in hits for item in sublist]
+    symbols = set()
+    # only keep aliases, gene names and HGNC symbols
+    for hit in hits:
+        for field in ["alias", "name", "symbol"]:
+            if field in hit and not "'" in hit[field]:
+                symbols.add(hit[field])
+    return list(symbols)
+
+
+def _has_annotation(genome):
+    """
+    Return True if a genome has an annotation for it, else False
+    """
+    providers = [
+        genomepy.ProviderBase.create(provider)
+        for provider in ["ensembl", "ucsc", "ncbi"]
+    ]
+    for provider in providers:
+        if genome in provider.genomes:
+            link = provider.get_annotation_download_link(genome)
+            return link is not None
+    return False
