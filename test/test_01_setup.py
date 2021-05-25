1fa921fc765311e5233e4097b5dfa14906b42e2c
Siebren Frölich <48289046+siebrenf@users.noreply.github.com>
2021-05-25 17:28:41 +0200

    Update test_01_setup.py

--- test/test_01_setup.py
+++ test/test_01_setup.py
@@ -7,12 +7,12 @@ travis = os.environ.get("TRAVIS") == "true"
 
 def test_black_formatting():
     if not travis:
-        sp.check_call("black setup.py genomepy/ tests/", shell=True)
+        sp.check_call("gimmemotifs/ setup.py scripts/", shell=True)
         pass
 
     try:
         sp.check_output(
-            "black --check setup.py genomepy/ tests/", stderr=sp.STDOUT, shell=True
+            "black --check gimmemotifs/ setup.py scripts/", stderr=sp.STDOUT, shell=True
         )
     except sp.CalledProcessError as e:
         msg = e.output.decode("utf-8")
@@ -27,7 +27,7 @@ def test_black_formatting():
 def test_flake8_formatting():
     try:
         sp.check_output(
-            "flake8 setup.py genomepy/ tests/", stderr=sp.STDOUT, shell=True
+            "flake8 setup.py gimmemotifs/ scripts/", stderr=sp.STDOUT, shell=True
         )
     except sp.CalledProcessError as e:
         msg = e.output.decode("utf-8")  # multi line error
