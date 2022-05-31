c7b563a8b0e7e973698fa5215b8cca6d61505067
siebrenf <siebrenf@gmail.com>
2022-05-31 22:27:13 +0200

    improve test_tools

--- test/test_tools.py
+++ test/test_tools.py
@@ -24,37 +24,28 @@ def ap1_included(motifs):
 @pytest.mark.parametrize("tool_name", __tools__)
 def test_tool(tool_name):
     """Test motif prediction tools."""
-    params = {"background": bg_fa, "organism": "hg38", "width": 7}
-    print(__tools__[tool_name])
-    if tool_name in [
-        # "amd",  # not installable via bioconda
-        # "bioprospector",  # not installable via bioconda
-        # "chipmunk",  # not installable via bioconda
-        "dreme",  # not installable via bioconda
-        # "hms",  # not installable via bioconda
-        # "improbizer",  # not installable via bioconda
-        # "mdmodule",  # not installable via bioconda
-        # "memew",  # not installable via bioconda
-        # "motifsampler",  # not installable via bioconda
-        "posmo",  # motif doesn't predictably look like AP1
-        "rpmcmc",  # not installable via bioconda
-
-        "gadem",  # sometimes crashes on invalid pointer
-        "jaspar",  # TODO: not configured
-        "xxmotif",  # takes too long
-        "trawler",  # takes too long + unpredictable, sometimes doesn't find the motif
-        "yamda",  # TODO: not configured
-    ]:
-        return
+    blacklist = {
+        "dreme": "not installable via bioconda/source",
+        "gadem": "sometimes crashes on invalid pointer",
+        "jaspar": "# TODO: not configured",  # TODO
+        "posmo": "motif doesn't predictably look like AP1",
+        "rpmcmc": "not installable via bioconda/source",
+        "trawler": "unpredictable, sometimes doesn't find the motif",
+        "xxmotif": "takes too long",
+        "yamda": "# TODO: not configured",  # TODO
+    }
+    if tool_name in blacklist:
+        pytest.skip(blacklist[tool_name])
 
     if platform.system() == "Darwin":
-        # No support for osx
-        if tool_name in ["amd", "hms", "improbizer", "motifsampler", "dinamo"]:
-            return
+        if tool_name in ["amd", "dinamo", "hms", "improbizer", "motifsampler"]:
+            pytest.skip("No supported for osx")
 
+    print("Tool class:", __tools__[tool_name])
     t = get_tool(tool_name)
     print("Testing {}...".format(t))
 
+    params = {"background": bg_fa, "organism": "hg38", "width": 7}
     (motifs, stderr, stdout) = t.run(fa, params)
     print(motifs)
     print(stderr)
