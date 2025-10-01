12dddc9866ef7e779fadfce3f041031609074fae
siebrenf <siebrenf@gmail.com>
2025-10-01 12:15:15 +0200

    install /w pyproject.toml

--- gimmemotifs/__init__.py
+++ gimmemotifs/__init__.py
@@ -6,6 +6,8 @@ from shutil import rmtree as _rmtree
 from tempfile import mkdtemp as _mkdtemp
 from warnings import warn as _warn
 
+from .__about__ import __version__
+
 
 def _filtered_warn(*args, **kwargs):
     """warnings.filterwarnings does not work."""
@@ -57,13 +59,6 @@ sh.setFormatter(screen_formatter)
 logger.addHandler(sh)
 del screen_formatter
 del sh
-
-
-# set __version__
-from ._version import get_versions  # noqa: E402
-
-__version__ = get_versions()["version"]
-del get_versions
 
 
 # import submodules to enable module autocomplete in editor
